69e37257c947358b3f65eb73a5e0d7ba06355065
altguerrero <josealberto.guerreroayala@gmail.com>
2024-07-16 20:10:39 -0500

    feat: add BookCard, BookList, BookContainer components
    - Added BookCard component for individual book display.
    - Added BookList component for displaying a grid of books using render props.
    - Added BookContainer component to integrate BookList and BookCard.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { NavbarLinks } from "@/types";
+import { Book, NavbarLinks } from "@/types";
 
 export const NAVBAR_LINKS: NavbarLinks[] = [
   {
@@ -15,5 +15,48 @@ export const NAVBAR_LINKS: NavbarLinks[] = [
     imgULR: "/assets/icons/library.svg",
     route: "/library",
     label: "Library",
+  },
+];
+
+export const FAKE_BOOKS: Book[] = [
+  {
+    id: 1,
+    title: "Book Title 1",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 2,
+    title: "Book Title 2",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 3,
+    title: "Book Title 3",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 4,
+    title: "Book Title 4",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 5,
+    title: "Book Title 5",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 6,
+    title: "Book Title 6",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 7,
+    title: "Book Title 7",
+    imgURL: "https://via.placeholder.com/180x270",
+  },
+  {
+    id: 8,
+    title: "Book Title 8",
+    imgURL: "https://via.placeholder.com/180x270",
   },
 ];
