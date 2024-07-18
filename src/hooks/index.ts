5d33257064df9f46697c2b28a2758cd1176e3b72
altguerrero <josealberto.guerreroayala@gmail.com>
2024-07-17 22:19:54 -0500

    Add Library view to display favorite books

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useBookSearch } from "./useBookSearch";
+export { default as useFavorites } from "./useFavorites";
 export { default as useGenericBookSearch } from "./useGenericBookSearch";
 export { default as useGoogleBookSearch } from "./useGoogleBookSearch";
 export { default as useOpenLibrarySearch } from "./useOpenLibrarySearch";
